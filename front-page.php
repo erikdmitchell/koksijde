e94637b3350b07f58a3eb0a77bd8e1a8f5bb4834
erikdmitchell <erikdmitchell@gmail.com>
2016-08-15 12:38:29 -0400

    removed title from front page template

--- front-page.php
+++ front-page.php
@@ -15,7 +15,6 @@
 		<div class="row">
 			<div class="col-md-12">
 				<?php while (have_posts()) : the_post(); ?>
-					<h2><?php the_title(); ?></h2>
 					<?php
 					if (has_post_thumbnail()) :
 						the_post_thumbnail('thumbnail');
