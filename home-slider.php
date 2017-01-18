a290ebbf4646946d7000a79e20334d57453ca822
Erik <erikdmitchell@gmail.com>
2017-01-18 16:43:08 -0500

    added function to check if slider is active
    fixes #11

--- home-slider.php
+++ home-slider.php
@@ -8,8 +8,10 @@
  */
 ?>
 
-<div id="<?php koksijde_slider_id(); ?>" class="carousel slide koksijde-slider" data-ride="carousel">
-	<?php koksijde_slider_indicators(); ?>
-	<?php koksijde_slider_slides(); ?>
-	<?php koksijde_slider_controls(); ?>
-</div>
+<?php if (koksijde_slider_is_active()) : ?>
+	<div id="<?php koksijde_slider_id(); ?>" class="carousel slide koksijde-slider" data-ride="carousel">
+		<?php koksijde_slider_indicators(); ?>
+		<?php koksijde_slider_slides(); ?>
+		<?php koksijde_slider_controls(); ?>
+	</div>
+<?php endif; ?>
