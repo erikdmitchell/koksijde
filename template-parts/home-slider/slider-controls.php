e8f31d2b0ef481cb584a179cacf5369ba5d527a0
Erik <erikdmitchell@gmail.com>
2017-01-19 16:56:33 -0500

    slider controls built out

--- template-parts/home-slider/slider-controls.php
+++ template-parts/home-slider/slider-controls.php
@@ -1,23 +1,10 @@
-/**
- * koksijde_slider_controls function.
- *
- * @access public
- * @return void
- */
-function koksijde_slider_controls() {
-	global $koksijde_slider_config;
+<?php if (get_theme_mod('home_slider_controls', 0)) : ?>
 
-	if (!count($koksijde_slider_config['posts']) || !$koksijde_slider_config['controls'])
-		return false;
+	<a class="left carousel-control" href="#'.$koksijde_slider_config['slider_id'].'" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+	</a>
+	<a class="right carousel-control" href="#'.$koksijde_slider_config['slider_id'].'" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+	</a>
 
-	$html=null;
-
-	$html.='<a class="left carousel-control" href="#'.$koksijde_slider_config['slider_id'].'" data-slide="prev">';
-		$html.='<span class="glyphicon glyphicon-chevron-left"></span>';
-	$html.='</a>';
-	$html.='<a class="right carousel-control" href="#'.$koksijde_slider_config['slider_id'].'" data-slide="next">';
-		$html.='<span class="glyphicon glyphicon-chevron-right"></span>';
-	$html.='</a>';
-
-	echo apply_filters('koksijde_slider_controls', $html);
-}
+<?php endif; ?>
