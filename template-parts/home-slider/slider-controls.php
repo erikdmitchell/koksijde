cb99f07e0a719f8a2c151929685bfdb91d3c7b19
Erik <erikdmitchell@gmail.com>
2017-03-22 15:13:15 -0400

    tweaked default slider controls setting

--- template-parts/home-slider/slider-controls.php
+++ template-parts/home-slider/slider-controls.php
@@ -1,5 +1,4 @@
-<?php if (get_theme_mod('home_slider_controls', 0)) : ?>
-
+<?php if (get_theme_mod('home_slider_controls', 1)) : ?>
 	<a class="left carousel-control" href="#'.$koksijde_slider_config['slider_id'].'" data-slide="prev">
 		<span class="glyphicon glyphicon-chevron-left"></span>
 	</a>
