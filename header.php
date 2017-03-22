b290dcc6c4307e0cb8c2f289403a4e0c92acf439
Erik <erikdmitchell@gmail.com>
2017-03-22 19:46:37 -0400

    added charset to header

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 <!--<![endif]-->
 
   <head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
