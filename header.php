3ea3afd8ca998cbedb99fd709a4b19e4a32f433c
Erik <erikdmitchell@gmail.com>
2017-03-22 12:28:26 -0400

    fixed potential translation issues
    fixes #23

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 				<nav class="navbar navbar-default" role="navigation">
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".koksijde-mobile-menu">
-							<span class="sr-only">Toggle navigation</span>
+							<span class="sr-only"><?php _e('Toggle navigation', 'koksijde'); ?></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
