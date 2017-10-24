37af06c2ca8e07e75696927ef0c0117209d33371
Erik <erikdmitchell@gmail.com>
2017-10-24 11:21:29 -0400

    fixed caption/slider image display issue

--- template-parts/home-slider/slide-captions.php
+++ template-parts/home-slider/slide-captions.php
@@ -1,7 +1,7 @@
 <?php if (get_theme_mod('slider_captions', 0)) : ?>
 
 	<div class="carousel-caption">
-		<div class="caption-text"><?php apply_filters('the_content', koksijde_slider_get_caption($post)); ?></div>
+		<div class="caption-text"><?php echo apply_filters('the_content', koksijde_slider_get_caption($post)); ?></div>
 
 	<?php if (get_theme_mod('slider_more_button', 0)) : ?>
 		<p><a class="btn btn-primary btn-lg" role="button"><?php echo esc_html(get_theme_mod('slider_read_more_text', '')); ?></a></p>
